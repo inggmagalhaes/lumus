a3afd86a711800876f7544bc6fa7bbdbf6bd047a
Akira Bassoli <akirabassoli@gmail.com>
2023-06-07 14:01:32 -0300

    atualização responsividade mobile

---
+++ menu.js
@@ -0,0 +1,15 @@
+
+function mudouTamanho() {
+    if (window.innerWidth >= 780) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
+function clickMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
